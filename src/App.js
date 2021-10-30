80278b38e9244024e643a349d4ccf8c4b14a8247
Bogosta <bogostamicheal@gmail.com>
2021-10-30 18:55:56 +0300

    mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {BsPlusCircle} from "react-icons/bs"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Analytics.js</h1>
+      <h2>Click the + button to add a new dataset</h2>
+
+      <input id="input" type = "file"></input>
+      <BsPlusCircle onClick = {() => {document.getElementsByTagName("input")[0].click()}} className = "AddButton" size = "3rem"></BsPlusCircle>
     </div>
   );
 }
